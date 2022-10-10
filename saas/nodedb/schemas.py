fd676f6bab55faacb2dce7e964e4b52409261ffe
heiko <aydt@arch.ethz.ch>
2022-10-10 18:04:01 +0800

    Sync files from main repo

--- saas/nodedb/schemas.py
+++ saas/nodedb/schemas.py
@@ -15,3 +15,5 @@ class NodeInfo(BaseModel):
     rti_service: bool = Field(..., title="RTI Service", description="Indicates if the node provides a RTI service.", example=True)
     p2p_address: Tuple[str, int] = Field(..., title="P2P Address", description="The address of the P2P service.", example=('127.0.0.1', 4001))
     rest_address: Optional[Tuple[str, int]] = Field(title="REST Address", description="The address of the REST service (if applicable - not all nodes have the REST interface enabled).", example=('127.0.0.1', 5001))
+    retain_job_history: Optional[bool] = Field(title="Retain History", description="Indicates if the node retains the job history (only applicable to full or execution nodes that offer RTI services).", example=True)
+    strict_deployment: Optional[bool] = Field(title="Strict Deployment", description="Indicates if the node restricts (un)deployment of processors to the node owner only (only applicable to full or execution nodes that offer RTI services).", example=True)
