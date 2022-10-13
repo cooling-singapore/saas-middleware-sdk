5e71c477cb4f3e48ddaedc08f0ff4ac2daa5ebf5
heiko <aydt@arch.ethz.ch>
2022-10-13 17:16:12 +0800

    Add initial SDK app base

---
+++ saas/sdk/app/auth.py
@@ -0,0 +1,153 @@
+import os.path
+from typing import Optional
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy import Column, String, Boolean, create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from jose import jwt
+
+from saas.core.helpers import generate_random_string
+from saas.core.keystore import Keystore
+from saas.rest.schemas import Token
+from saas.sdk.app.exceptions import AppRuntimeError
+
+Base = declarative_base()
+
+
+class UserRecord(Base):
+    __tablename__ = 'user'
+    username = Column(String(16), primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    disabled = Column(Boolean, nullable=False)
+    keystore_id = Column(String(64), nullable=False)
+    keystore_password = Column(String, nullable=False)
+    hashed_password = Column(String(64), nullable=False)
+
+
+class User(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
+    username: str
+    name: str
+    email: str
+    disabled: bool
+    hashed_password: str
+    keystore: Keystore
+
+
+class UserDB:
+    _keystore_path = None
+    _engine = None
+    _Session = None
+    _keystores = {}
+
+    @classmethod
+    def initialise(cls, wd_path: str):
+        # create directories
+        cls._keystore_path = os.path.join(wd_path, 'keystores')
+        os.makedirs(cls._keystore_path, exist_ok=True)
+
+        # initialise database things
+        db_path = f"sqlite:///{os.path.join(wd_path, 'user.db')}"
+        cls._engine = create_engine(db_path)
+        Base.metadata.create_all(cls._engine)
+        cls._Session = sessionmaker(bind=cls._engine)
+
+    @classmethod
+    def get_user(cls, username: str) -> Optional[User]:
+        with cls._Session() as session:
+            record = session.query(UserRecord).get(username)
+            if record:
+                if record.username not in cls._keystores:
+                    # load the keystore
+                    keystore_path = os.path.join(cls._keystore_path, f"{record.keystore_id}.json")
+                    cls._keystores[record.username] = Keystore.load(keystore_path, record.keystore_password)
+
+                return User(
+                    username=record.username,
+                    name=record.name,
+                    email=record.email,
+                    disabled=record.disabled,
+                    hashed_password=record.hashed_password,
+                    keystore=cls._keystores[record.username]
+                )
+
+            else:
+                return None
+
+    @classmethod
+    def add_user(cls, username: str, name: str, email: str, password: str):
+        with cls._Session() as session:
+            # check if this username already exists
+            record = session.query(UserRecord).get(username)
+            if record:
+                raise AppRuntimeError("Username already exists", details={
+                    'username': username
+                })
+
+            # create a new keystore
+            keystore_password = generate_random_string(16)
+            keystore = Keystore.create(cls._keystore_path, name, email, keystore_password)
+
+            # add new user (with a randomly generated password)
+            session.add(UserRecord(username=username, name=name, email=email, disabled=False,
+                                   keystore_id=keystore.identity.id, keystore_password=keystore_password,
+                                   hashed_password=UserAuth.get_password_hash(password)))
+            session.commit()
+
+
+class UserAuth:
+    secret_key = None
+    algorithm = 'HS256'
+    _access_token_expires_minutes = 30
+    _pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    @classmethod
+    def initialise(cls, secret_key: str):
+        cls.secret_key = secret_key
+
+    @classmethod
+    def _verify_password(cls, plain_password: str, hashed_password: str) -> bool:
+        return cls._pwd_context.verify(plain_password, hashed_password)
+
+    @classmethod
+    def _authenticate_user(cls, username: str, password: str) -> Optional[User]:
+        user = UserDB.get_user(username)
+        if not user:
+            return None
+
+        if not cls._verify_password(password, user.hashed_password):
+            return None
+
+        return user
+
+    @classmethod
+    def get_password_hash(cls, password: str) -> str:
+        return cls._pwd_context.hash(password)
+
+    @classmethod
+    async def login_for_access_token(cls, form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+        # get the user
+        user = cls._authenticate_user(form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        # create the token
+        expiry = datetime.utcnow() + timedelta(minutes=cls._access_token_expires_minutes)
+        content = {
+            'sub': user.username,
+            'exp': expiry
+        }
+        access_token = jwt.encode(content, cls.secret_key, algorithm=cls.algorithm)
+
+        return Token(access_token=access_token, token_type='bearer', expiry=expiry.timestamp())
---
+++ saas/sdk/app/base.py
@@ -0,0 +1,185 @@
+from __future__ import annotations
+
+import abc
+from threading import Lock
+from typing import List, Union
+
+import uvicorn
+from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
+from threading import Thread
+
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
+from saas.core.exceptions import SaaSRuntimeException
+from saas.core.logging import Logging
+from saas.rest.exceptions import UnsupportedRESTMethod
+from saas.rest.schemas import EndpointDefinition, Token
+from saas.sdk.app.auth import UserAuth, UserDB, User
+
+logger = Logging.get('saas.sdk.app')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, UserAuth.secret_key, algorithms=[UserAuth.algorithm])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # try to get the user
+    user = UserDB.get_user(username=token_data.username)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+class Application(abc.ABC):
+    def __init__(self, address: (str, int), endpoint_prefix: str, wd_path: str, title: str, version: str,
+                 description: str) -> None:
+
+        self._address = address
+        self._endpoint_prefix = endpoint_prefix
+        self._wd_path = wd_path
+        self._title = title
+        self._version = version
+        self._description = description
+
+        self._mutex = Lock()
+        self._api = FastAPI()
+        self._thread = None
+
+    def _register(self, endpoint: EndpointDefinition) -> None:
+        route = f"{endpoint.prefix}/{endpoint.rule}"
+        logger.info(f"REST app is mapping {endpoint.method}:{route} to {endpoint.function}")
+        if endpoint.method == 'POST':
+            self._api.post(route,
+                           response_model=endpoint.response_model,
+                           dependencies=endpoint.dependencies,
+                           description=endpoint.function.__doc__)(endpoint.function)
+        elif endpoint.method == 'GET':
+            self._api.get(route,
+                          response_model=endpoint.response_model,
+                          dependencies=endpoint.dependencies,
+                          description=endpoint.function.__doc__)(endpoint.function)
+        elif endpoint.method == 'PUT':
+            self._api.put(route,
+                          response_model=endpoint.response_model,
+                          dependencies=endpoint.dependencies,
+                          description=endpoint.function.__doc__)(endpoint.function)
+        elif endpoint.method == 'DELETE':
+            self._api.delete(route,
+                             response_model=endpoint.response_model,
+                             dependencies=endpoint.dependencies,
+                             description=endpoint.function.__doc__)(endpoint.function)
+        else:
+            raise UnsupportedRESTMethod(endpoint.method, route)
+
+    async def _close(self) -> None:
+        logger.info(f"REST app is shutting down.")
+
+    @property
+    def address(self) -> (str, int):
+        return self._address
+
+    @property
+    def endpoint_prefix(self) -> str:
+        return self._endpoint_prefix
+
+    @abc.abstractmethod
+    def endpoints(self) -> List[EndpointDefinition]:
+        pass
+
+    def startup(self) -> None:
+        if self._thread is None:
+            self._api.on_event("shutdown")(self._close)
+
+            # collect endpoints
+            endpoints = self.endpoints()
+            endpoints.append(EndpointDefinition('POST', '', 'token', UserAuth.login_for_access_token, Token, None))
+
+            # add endpoints
+            for endpoint in endpoints:
+                self._register(endpoint)
+
+            # update the openapi schema
+            self._api.openapi_schema = get_openapi(
+                title=self._title,
+                version=self._version,
+                description=self._description,
+                routes=self._api.routes
+            )
+
+            @self._api.exception_handler(SaaSRuntimeException)
+            async def exception_handler(_: Request, exception: SaaSRuntimeException):
+                return JSONResponse(
+                    status_code=500,
+                    content={
+                        'reason': exception.reason,
+                        'id': exception.id,
+                        'details': exception.details
+                    }
+                )
+
+            # setup CORS
+            self._api.add_middleware(
+                CORSMiddleware,
+                allow_origins=['*'],
+                allow_credentials=True,
+                allow_methods=["*"],
+                allow_headers=["*"],
+            )
+
+            logger.info(f"REST service starting up...")
+            self._thread = Thread(target=uvicorn.run, args=(self._api,),
+                                  kwargs={"host": self._address[0], "port": self._address[1], "log_level": "info"},
+                                  daemon=True)
+
+            self._thread.start()
+            # await asyncio.sleep(0.1)
+
+        else:
+            logger.warning(f"REST service asked to start up but thread already exists! Ignoring...")
+
+    def shutdown(self) -> None:
+        if self._thread is None:
+            logger.warning(f"REST service asked to shut down but thread does not exist! Ignoring...")
+
+        else:
+            logger.info(f"REST service shutting down...")
+            # there is no way to terminate a thread...
+            # self._thread.terminate()
